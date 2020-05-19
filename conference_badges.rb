79941a808e6362bfe12a4ef508bfeb6eddda6c9e
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-19 19:39:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def conference_badges(names)
+
+
+puts "Hello, my name is #{name}."
+end
+
+return array of names
+end 
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
