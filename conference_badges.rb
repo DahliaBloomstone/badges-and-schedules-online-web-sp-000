791907c9d38cbc505df528d9d6a318a0904e98e2
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-19 19:41:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,4 +12,8 @@ names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def assign_rooms
 
 puts "Hello, #{}! You'll be assigned to room #{}!"
+end
+
+def printer
+  return methods above
 end 
