f3c6f9ece635edf28c467fd5b455ee121b6e5051
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-19 19:40:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,11 @@ puts "Hello, my name is #{name}."
 end
 
 return array of names
-end 
+end
 
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def assign_rooms
+
+puts "Hello, #{}! You'll be assigned to room #{}!"
+end 
